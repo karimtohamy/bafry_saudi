0485345f80dac35620ff51f5590de1becdfd0bd8
karim.bassem <karim.bassem@exponile.com>
2024-10-31 14:20:49 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/bafry_saudi/",
   plugins: [
     vue(),
     vueDevTools(),
