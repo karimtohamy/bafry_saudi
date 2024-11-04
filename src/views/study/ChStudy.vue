60628f86f4cddf7ca4eec45db3d23e6b19b30cdc
karim.bassem <karim.bassem@exponile.com>
2024-11-04 13:02:17 +0200

    studyies finished

---
+++ src/views/study/ChStudy.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="flex justify-center flex-col items-center px-1">
+        <h2 class="my-5 text-2xl font-semibold text-center">COMPARISON OF D-H/S IONS WITH CHLORINE</h2>
+        <div class="shadow-lg rounded overflow-hidden max-w-full">
+            <table class="max-w-full border-collapse border border-gray-300">
+                <thead>
+                    <tr class="bg-gray-200">
+                        <th class="border border-gray-300 p-2 text-center">Property</th>
+                        <th class="border border-gray-300 p-2 text-center">D-H/S IONS</th>
+                        <th class="border border-gray-300 p-2 text-center">CHLORINE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Long term effect</td>
+                        <td class="border border-gray-300 p-2">very long</td>
+                        <td class="border border-gray-300 p-2">depending on temperature, short till medium</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Light sensibility</td>
+                        <td class="border border-gray-300 p-2">practically not</td>
+                        <td class="border border-gray-300 p-2">Medium</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Temperature sensibility</td>
+                        <td class="border border-gray-300 p-2">practically not</td>
+                        <td class="border border-gray-300 p-2">Fast degradation at rising temperatures</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Neutral pH</td>
+                        <td class="border border-gray-300 p-2">neutral</td>
+                        <td class="border border-gray-300 p-2">change of pH of the treated water</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Contact time</td>
+                        <td class="border border-gray-300 p-2">medium</td>
+                        <td class="border border-gray-300 p-2">Short</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Retarded efficiency through organic materials</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Influence of ammonia or urea</td>
+                        <td class="border border-gray-300 p-2">till 5 mg/l not</td>
+                        <td class="border border-gray-300 p-2">reaction to chloramines</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Efficiency against algae</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                        <td class="border border-gray-300 p-2">Limited</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Efficiency against fungi</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                        <td class="border border-gray-300 p-2">Limited</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Forming of smell</td>
+                        <td class="border border-gray-300 p-2">No</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Change of taste</td>
+                        <td class="border border-gray-300 p-2">No</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Danger of over dose</td>
+                        <td class="border border-gray-300 p-2">practically not</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                    </tr>
+                    <tr class="hover:bg-gray-200">
+                        <td class="border border-gray-300 p-2">Carcinogenic/mutagenic</td>
+                        <td class="border border-gray-300 p-2">No</td>
+                        <td class="border border-gray-300 p-2">reaction: yes</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* Add any additional scoped styles here if necessary */
+</style>
---
+++ src/views/study/acidStudy.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="flex justify-center flex-col items-center mb-6 mx-2">
+        <h1 class="my-5 text-2xl font-semibold text-center">COMPARISON OF D-H/S IONS WITH PERACETIC ACID</h1>
+
+
+        <div class="overflow-x-auto">
+            <table class="max-w-full border-collapse border border-gray-300 mt-4">
+                <thead>
+                    <tr class="bg-gray-200">
+                        <th class="border border-gray-300 p-2">Property</th>
+                        <th class="border border-gray-300 p-2">D-H/S IONS</th>
+                        <th class="border border-gray-300 p-2">PERACETIC ACID</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Universal usage</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                        <td class="border border-gray-300 p-2">No</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Gaps of effect</td>
+                        <td class="border border-gray-300 p-2">No</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Smell</td>
+                        <td class="border border-gray-300 p-2">Practically no</td>
+                        <td class="border border-gray-300 p-2">Biting</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Influence on taste, smell & consistence of food /
+                            beverages</td>
+                        <td class="border border-gray-300 p-2">No</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Rinsing necessary</td>
+                        <td class="border border-gray-300 p-2">No</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Corrosive</td>
+                        <td class="border border-gray-300 p-2">No</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Stacking</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                        <td class="border border-gray-300 p-2">Limited</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Fully automatic feedback-control</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                        <td class="border border-gray-300 p-2">No</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Application range of pH</td>
+                        <td class="border border-gray-300 p-2">1 - 8</td>
+                        <td class="border border-gray-300 p-2">2.5 - 4</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Application range of temperature</td>
+                        <td class="border border-gray-300 p-2">5° - 95°C</td>
+                        <td class="border border-gray-300 p-2">5° - 40°C; Danger of explosion > 65°C</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Neutralisation of waste water</td>
+                        <td class="border border-gray-300 p-2">No</td>
+                        <td class="border border-gray-300 p-2">Yes</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-300 p-2">Velocity of killing of germs</td>
+                        <td class="border border-gray-300 p-2">Medium</td>
+                        <td class="border border-gray-300 p-2">Fast</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
