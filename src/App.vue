fc2b56226451174c91fbb8a70d8f2c435c3c7201
karim.bassem <karim.bassem@exponile.com>
2024-11-04 12:57:46 +0200

    added sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-	<div class="h-full flex">
+	<div class="h-full flex min-w-full">
+		<Sidebar :class="{ '-ms-[250px] transition-all': !sidebarOpen }" @toggle-sidebar="toggleSidebar" />
+		<div class="flex-1 ">
+			<Navbar @toggle-sidebar="toggleSidebar" class="fixed z-10 w-full" />
 
-		<Navbar />
 
-		<main class="">
-			<div class="min-h-[10vh]">
+			<main class=" pt-12">
 				<router-view></router-view>
-			</div>
-
-		</main>
+			</main>
+		</div>
 	</div>
 </template>
 
@@ -16,6 +16,7 @@
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline';
 import Navbar from './components/ui/navbar/navbar.vue';
 import { ref, onMounted, onUnmounted } from 'vue';
+import Sidebar from './components/ui/sidebar/sidebar.vue';
 
 const navigation = [
 	{ name: '', href: '', current: true },
@@ -24,9 +25,52 @@ const navigation = [
 	{ name: '', href: '', current: false },
 ]
 
+const sidebarOpen = ref(false)
+function toggleSidebar(){
+	sidebarOpen.value = !sidebarOpen.value
+	console.log('loging',sidebarOpen.value);
+	
+}
 // Define a reactive variable for tracking scroll state
 
 </script>
 
 
-<style scoped></style>
+<style scoped>
+/** {
+  scrollbar-width: thin;
+  scrollbar-color: #397524 #EBEBEB;
+}*/
+
+/* Chrome, Edge and Safari */
+*::-webkit-scrollbar {
+	height: 9px;
+	width: 9px;
+}
+
+*::-webkit-scrollbar-track {
+	border-radius: 0px;
+	background-color: #EBEBEB;
+}
+
+*::-webkit-scrollbar-track:hover {
+	background-color: #FF0000;
+}
+
+*::-webkit-scrollbar-track:active {
+	background-color: #C2C2C2;
+}
+
+*::-webkit-scrollbar-thumb {
+	border-radius: 12px;
+	background-color: #397524;
+}
+
+*::-webkit-scrollbar-thumb:hover {
+	background-color: #62A34B;
+}
+
+*::-webkit-scrollbar-thumb:active {
+	background-color: #62A34B;
+}
+</style>
