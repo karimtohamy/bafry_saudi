198d1d8dff67c93517938b1008236c1edf06264a
karim.bassem <karim.bassem@exponile.com>
2024-11-15 13:55:21 +0200

    removed bg image

--- src/views/study/studies.vue
+++ src/views/study/studies.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="background h-screen flex items-center justify-center px-10"
-        :style="{ backgroundImage: `url(${backgroundImage})` }">
+    <div class="background h-screen flex items-center justify-center px-10 bg-main">
         <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 w-full gap-8">
             <RouterLink v-for="study in studies" :key="study.to" :to="{ name: study.to }"
                 class="shadow-lg rounded flex items-center justify-center p-5 bg-sec text-white hover:scale-110 transition-all">
