45cc9f44e472dcb493ef371110d09dbf146c20e5
karim.bassem <karim.bassem@exponile.com>
2024-11-15 13:59:40 +0200

    mission and vision

--- src/components/mission.vue
+++ src/components/mission.vue
@@ -1,43 +1,30 @@
 <template>
-    <div >
-        <div class=" w-full h-[90vh]">
-            <div class=" h-1/2  w-full flex items-center relative overflow-hidden ">
-                <img :src="mountainImage" alt=""
-                    class="w-full img1 overflow-hidden h-auto object-cover  object-bottom " />
-                <div class="absolute right-0 top-1/2">
-                    dndhu dughghg yf trvtybtrv
-                </div>
+    <div
+        class="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-12 px-12 items-center justify-center w-full  h-auto p-10">
+        <div
+            class="shadow-lg py-10 w-full  rounded-lg flex px-3 mx-auto text-center  flex-col bg-slate-300/10 text-black jsutify-center items-center">
 
-            </div>
-            <div class="h-1/2 flex justify-end w-full  overflow-hidden relative ">
-                <div class="absolute left-0 top-1/2">
-                    dndhu dughghg yf trvtybtrv
-                </div>
-                <img :src="beachImage" alt="" class="w-full  object-cover object-bottom img2 " />
+            <img src="@/assets/vision.svg" alt="" class="size-10 mb-2" />
+            <h1 class="text-2xl font-semibold mb-3">Vision</h1>
 
-            </div>
+
+            <p class="text-xl">To lead the way in environmental solutions, offering a wide range of products
+                designed to
+                address unique application needs, making a lasting impact for a cleaner, safer environment.
+            </p>
+        </div>
+        <div
+            class="shadow-lg py-10 w-full h-full rounded-lg flex px-3 mx-auto text-center  flex-col bg-slate-300/10 text-black jsutify-center items-center">
+            <img src="@/assets/target-icon.svg" alt="" class="size-10 mb-2">
+
+            <h1 class="text-2xl font-semibold mb-3">Mission</h1>
+            <p class="text-xl">To create a microbe-free world and elevate environmental health standards through
+                innovative solutions tailored to diverse environmental challenges.</p>
         </div>
     </div>
 </template>
 
 <script setup>
-import mountainImage from '@/assets/images/mountain.jpg';
-import beachImage from '@/assets/images/beach_big_copped.jpg';
 </script>
 
-<style scoped>
-.img1 {
-clip-path: polygon(40.2% 100%, 0% 100%, 0% 0%, 70% 0%);
-
-
-
-}
-
-
-
-.img2 {
-    clip-path: polygon(50% 0, 100% 0%, 100% 100%, 40% 100%);
-    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.3);
-        /* Custom shadow */
-}
-</style>
+<style scoped></style>
