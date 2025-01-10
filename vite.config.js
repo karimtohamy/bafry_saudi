9fe497276f7f3eb05ad936d2115df4c52046519c
karimtohamy <karimbassemtohamy@gamil.com>
2025-01-10 21:21:13 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import Prerender from 'vite-plugin-prerender';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
