4171cb9415a0f63dff01ed27f6a4834cc06c283d
karimtohamy <karimbassemtohamy@gamil.com>
2025-01-10 21:24:56 +0200

    perender removed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-import Prerender from 'vite-plugin-prerender';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -9,30 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    Prerender({
-      // List the routes you want to pre-render
-      routes: [
-        '/en/home',
-        '/en/products',
-        '/en/studies',
-        '/en/studies/chlorine-study',
-        '/en/studies/peracetic-acid-study',
-        '/en/studies/study-in-fish',
-        '/en/faq',
-        '/en/contact-us',
-        '/en/tools',
-        '/ar/home',
-        '/ar/products',
-        '/ar/studies',
-        '/ar/studies/chlorine-study',
-        '/ar/studies/peracetic-acid-study',
-        '/ar/studies/study-in-fish',
-        '/ar/faq',
-        '/ar/contact-us',
-        '/ar/tools',
-      ],
-      outputDir: 'dist', 
-    })
+  
   ],
   
   resolve: {
