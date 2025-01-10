3dd0a4bfc52ca07dd736abf2ccd5ef2a39c25c20
karimtohamy <karimbassemtohamy@gamil.com>
2025-01-10 18:32:59 +0200

    vte config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/bafry_saudi/',
   plugins: [
     vue(),
     vueDevTools(),
