0b12cf6160378c8c2b7b6505b8074e98b5f635f8
karimtohamy <karimbassemtohamy@gamil.com>
2025-01-10 21:18:49 +0200

    seo

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,32 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Prerender({
+      // List the routes you want to pre-render
+      routes: [
+        '/en/home',
+        '/en/products',
+        '/en/studies',
+        '/en/studies/chlorine-study',
+        '/en/studies/peracetic-acid-study',
+        '/en/studies/study-in-fish',
+        '/en/faq',
+        '/en/contact-us',
+        '/en/tools',
+        '/ar/home',
+        '/ar/products',
+        '/ar/studies',
+        '/ar/studies/chlorine-study',
+        '/ar/studies/peracetic-acid-study',
+        '/ar/studies/study-in-fish',
+        '/ar/faq',
+        '/ar/contact-us',
+        '/ar/tools',
+      ],
+      outputDir: 'dist', 
+    })
   ],
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
